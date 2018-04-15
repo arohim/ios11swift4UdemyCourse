756c641abe5e9ee39c431130509b620f11d22956
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2018-04-15 22:01:32 +0700

    Done 42. App: Swoosh 05 - Intro to segues (changing screens)

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -18,12 +18,10 @@ class ViewController: UIViewController {
         
 //        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindFromSkillVC(unwindSeque: UIStoryboardSegue) {
+        
     }
-
 
 }
 
