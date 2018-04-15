6111093e3d6b5d1a0e06d8b723def969d58f3a8e
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2018-04-15 21:19:45 +0700

    Done App: Swoosh 03 - Intro to auto layout (support multiple screen sizes)  App: Swoosh 04 - Working with stack views

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var swoosh: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size)
     }
 
     override func didReceiveMemoryWarning() {
