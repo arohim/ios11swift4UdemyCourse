bd41f6b11285d2032f512807d0f00e8c400ba100
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2018-04-21 22:59:00 +0700

    Done App: Dev Profile 01 - Auto layout for iPhones Section 4, Lecture 48

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Abdulrohim 'Him' Sama on 21/4/2561 BE.
+//  Copyright © 2561 Abdulrohim 'Him' Sama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
