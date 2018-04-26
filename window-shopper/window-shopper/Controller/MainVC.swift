472c2c4465c922573c0329fe4f5c5229a3619f5f
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2018-04-26 23:27:45 +0700

    Done - 53. App: Window Shopper 04 - Calculation algorithm

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -12,6 +12,8 @@ class MainVC: UIViewController {
 
     @IBOutlet weak var wageTxt: CurrencyTxtField!
     @IBOutlet weak var priceTxt: CurrencyTxtField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hourLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +27,29 @@ class MainVC: UIViewController {
         
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hourLbl.isHidden = true
     }
     
     @objc func calculate() {
-        print("Calculate")
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                
+                resultLbl.isHidden = false
+                hourLbl.isHidden = false
+                
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
 
+    @IBAction func clearBtnWasPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hourLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
+    }
 }
 
