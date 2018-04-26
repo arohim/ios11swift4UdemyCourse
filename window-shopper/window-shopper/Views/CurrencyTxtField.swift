1cce6f887b16da81056dbb2126c2bb765659f6b4
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2018-04-26 23:37:54 +0700

    Done - 54. App: Window Shopper 05 - Custom drawing with drawRect

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -10,8 +10,24 @@ import UIKit
 
 @IBDesignable
 class CurrencyTxtField: UITextField {
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currentcyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        
+        currentcyLbl.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        currentcyLbl.textAlignment = .center
+        currentcyLbl.textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        currentcyLbl.layer.cornerRadius = 5.0
+        currentcyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currentcyLbl.text = formatter.currencySymbol
+        addSubview(currentcyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
-        awakeFromNib()
+        customizeView()
     }
     
     override func awakeFromNib() {
@@ -23,6 +39,8 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.45)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
