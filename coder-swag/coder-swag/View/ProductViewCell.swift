53f8df744223f8f2b1750e6d65aef2f84c7cdac3
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2018-05-08 23:00:07 +0700

    App: Coder Swag 03 - Collection views (grid layouts)

---
+++ coder-swag/coder-swag/View/ProductViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductViewCell.swift
+//  coder-swag
+//
+//  Created by Abdulrohim 'Him' Sama on 8/5/2561 BE.
+//  Copyright © 2561 Abdulrohim 'Him' Sama. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+}
